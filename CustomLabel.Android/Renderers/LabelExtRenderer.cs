4bf63955467288bf2fe718f3ce7b2ed35acd9b98
Jovy Vallamattam <ltt1jxv@ups.com>
2019-06-21 12:29:19 -0400

    First commit

---
+++ CustomLabel.Android/Renderers/LabelExtRenderer.cs
@@ -0,0 +1,85 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Android.Widget;
+using CustomLabel.Controls;
+using CustomLabel.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(LabelExt), typeof(LabelExtRenderer))]
+namespace CustomLabel.Droid.Renderers
+{
+    public class LabelExtRenderer : ViewRenderer<LabelExt, Android.Views.View>
+    {
+        private Context _context;
+        private TextView _textView;
+        private LabelExt _element;
+        private Android.Views.View _rootLayout;
+        public LabelExtRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<LabelExt> e)
+        {
+            base.OnElementChanged(e);
+            _element = Element as LabelExt;
+            if (Control == null)
+            {
+                // Inflate the IncrementingButton layout
+                var inflater = _context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+                _rootLayout = inflater.Inflate(Resource.Layout.label_view, null, false);
+                _textView = _rootLayout.FindViewById<TextView>(Resource.Id.textView1);
+
+                // Tell Xamarin to user our layout for the control
+                SetNativeControl(_rootLayout);
+                SetBackground(_rootLayout);
+            }
+
+            SetControlProperties();
+
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (_textView != null)
+            {
+                if (e.PropertyName == "Text")
+                {
+                    _textView.Text = _element.Text;
+                }
+            }
+        }
+
+        private void SetControlProperties()
+        {
+            _textView.Text = _element.Text;
+            _textView.SetTextColor(_element.TextColor.ToAndroid());
+            if (_element.FontSize > 0)
+                _textView.TextSize = (float)_element.FontSize;
+           
+             
+        }
+
+        private void SetBackground(Android.Views.View rootLayout)
+        {
+            // Get the background color from Forms element
+            var backgroundColor = Element.BackgroundColor.ToAndroid();
+
+            // Create statelist to handle ripple effect
+            var enabledBackground = new GradientDrawable(GradientDrawable.Orientation.LeftRight, new int[] { backgroundColor, backgroundColor });
+            var stateList = new StateListDrawable();
+            var rippleItem = new RippleDrawable(ColorStateList.ValueOf(Android.Graphics.Color.White),
+                                                enabledBackground,
+                                                null);
+            stateList.AddState(new[] { Android.Resource.Attribute.StateEnabled }, rippleItem);
+
+            // Assign background
+            rootLayout.Background = stateList;
+            rootLayout.Clickable = true;
+        }
+    }
+}
---
+++ CustomLabel.iOS/Renderers/LabelExtRenderer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using CoreGraphics;
+using CustomLabel.Controls;
+using CustomLabel.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(LabelExt), typeof(LabelExtRenderer))]
+namespace CustomLabel.iOS.Renderers
+{
+    public class LabelExtRenderer: ViewRenderer<LabelExt, UIView>
+    {
+        private UILabel _buttonTitleLabel;
+        private UIButton _rootView;
+        protected override void OnElementChanged(ElementChangedEventArgs<LabelExt> e)
+        {
+            base.OnElementChanged(e);
+        
+            _rootView = CreateLayout();
+
+            AddTouchBehavior(_rootView);
+
+            // Tell Xamarin to user our layout for the control
+            SetNativeControl(_rootView);
+            //var dim = _buttonTitleLabel.Text.StringSize(_buttonTitleLabel.Font);
+
+            //rootView.Frame = new CoreGraphics.CGRect(0, 0, dim.Width, dim.Height);
+
+        }
+
+        private UIButton CreateLayout()
+        {
+            var rootView = new UIButton (UIButtonType.Plain)
+            {
+
+                UserInteractionEnabled = true,
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                BackgroundColor = Color.Wheat.ToUIColor(),
+                //TitleLabel.Text = Element.Text,
+               // TextColor = Element.TextColor.ToUIColor(),
+            };
+
+            // Create button title label
+            _buttonTitleLabel = rootView.TitleLabel;
+
+            _buttonTitleLabel.Text = Element.Text;
+            _buttonTitleLabel.TextColor = Element.TextColor.ToUIColor();
+            //    UserInteractionEnabled = false,
+            //    Text = Element.Text,
+            //    TextColor = Element.TextColor.ToUIColor(),
+            //    TranslatesAutoresizingMaskIntoConstraints = false,
+            //    LineBreakMode = UILineBreakMode.WordWrap,
+            //    Lines = 0,
+                
+            //};
+
+            
+
+            //rootView.AddSubview(_buttonTitleLabel);
+
+          
+
+          
+            return rootView;
+        }
+        private void AddTouchBehavior(UIButton rootView)
+        {
+            rootView.TouchDown += AddTouchBackgroundColor;
+            rootView.TouchDragEnter += AddTouchBackgroundColor;
+            rootView.TouchUpInside += RemoveTouchBackgroundColor;
+            rootView.TouchCancel += RemoveTouchBackgroundColor;
+            rootView.TouchDragExit += RemoveTouchBackgroundColor;
+        }
+
+        private void AddTouchBackgroundColor(object sender, EventArgs e)    
+        {
+            (sender as UIView).BackgroundColor = Element.BackgroundColor.ToUIColor().ColorWithAlpha(.5f);
+        }
+
+        private void RemoveTouchBackgroundColor(object sender, EventArgs e)
+        {
+            (sender as UIView).BackgroundColor = Element.BackgroundColor.ToUIColor();
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            var lblConstraints = new[]
+          {
+                _rootView.TopAnchor.ConstraintEqualTo (_buttonTitleLabel.TopAnchor, -2),
+                _rootView.LeftAnchor.ConstraintEqualTo (_buttonTitleLabel.LeftAnchor),
+                _rootView.RightAnchor.ConstraintEqualTo (_buttonTitleLabel.RightAnchor, 2),
+                _rootView.HeightAnchor.ConstraintEqualTo (_buttonTitleLabel.HeightAnchor),
+                _rootView.TrailingAnchor.ConstraintEqualTo (_buttonTitleLabel.TrailingAnchor),
+
+            };
+            NSLayoutConstraint.ActivateConstraints(lblConstraints);
+
+        }
+
+    }
+}

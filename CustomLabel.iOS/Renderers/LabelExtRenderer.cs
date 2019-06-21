812858670764e8d7bdf0d73d7bb42c1f7f8529d0
Jovy Vallamattam <ltt1jxv@ups.com>
2019-06-21 13:24:07 -0400

    reverted ios renderer

--- CustomLabel.iOS/Renderers/LabelExtRenderer.cs
+++ CustomLabel.iOS/Renderers/LabelExtRenderer.cs
@@ -23,10 +23,7 @@ namespace CustomLabel.iOS.Renderers
 
             // Tell Xamarin to user our layout for the control
             SetNativeControl(_rootView);
-            //var dim = _buttonTitleLabel.Text.StringSize(_buttonTitleLabel.Font);
-
-            //rootView.Frame = new CoreGraphics.CGRect(0, 0, dim.Width, dim.Height);
-
+            
         }
 
         private UIButton CreateLayout()
@@ -37,31 +34,28 @@ namespace CustomLabel.iOS.Renderers
                 UserInteractionEnabled = true,
                 TranslatesAutoresizingMaskIntoConstraints = false,
                 BackgroundColor = Color.Wheat.ToUIColor(),
-                //TitleLabel.Text = Element.Text,
-               // TextColor = Element.TextColor.ToUIColor(),
+                
             };
 
             // Create button title label
-            _buttonTitleLabel = rootView.TitleLabel;
+            _buttonTitleLabel =  new UILabel {
 
-            _buttonTitleLabel.Text = Element.Text;
-            _buttonTitleLabel.TextColor = Element.TextColor.ToUIColor();
-            //    UserInteractionEnabled = false,
-            //    Text = Element.Text,
-            //    TextColor = Element.TextColor.ToUIColor(),
-            //    TranslatesAutoresizingMaskIntoConstraints = false,
-            //    LineBreakMode = UILineBreakMode.WordWrap,
-            //    Lines = 0,
-                
-            //};
+                UserInteractionEnabled = false,
+                Text = Element.Text,
+                TextColor = Element.TextColor.ToUIColor(),
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                LineBreakMode = UILineBreakMode.WordWrap,
+                Lines = 0,
 
-            
+            };
 
-            //rootView.AddSubview(_buttonTitleLabel);
 
-          
 
-          
+            rootView.AddSubview(_buttonTitleLabel);
+
+
+
+
             return rootView;
         }
         private void AddTouchBehavior(UIButton rootView)
